ff8cbc4c2e4194f3ed46541b60d2cf62810bedf8
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-06-30 23:37:17 +0530

    routes-fixed

--- Week-5/routes/userRoutes.js
+++ Week-5/routes/userRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
 
-router.post('/users', userController.createUser);
-router.get('/users/:id', userController.getUserById);
-router.put('/users/:id', userController.updateUser);
-router.delete('/users/:id', userController.deleteUser);
+router.post('/createUser', userController.createUser);
+router.get('/getUserById/:id', userController.getUserById);
+router.put('/updateUser/:id', userController.updateUser);
+router.delete('/deleteUser/:id', userController.deleteUser);
 router.get('/users/search', userController.searchUsers);
 
 
